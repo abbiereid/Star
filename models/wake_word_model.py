bdecbc5d5c36b74e6ae7ff10469e9676f04378e1
abbiereid <abbiereid222@gmail.com>
2024-11-19 04:09:43 +0000

    started to add gesture recognition

--- models/wake_word_model.py
+++ models/wake_word_model.py
@@ -1,15 +1,20 @@
 import mediapipe as mp
 import cv2
 import numpy as np
-import uuid
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from camera import Camera
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+
+def gesture_recognition(image):
+    return
+    
 
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     camera = Camera()
@@ -18,7 +23,10 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         camera.capture()
         
         image = cv2.cvtColor(camera.get_frame(), cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1)
         image.flags.writeable = False
+
+        print(gesture_recognition(image))
 
         try:
             results = hands.process(image)
