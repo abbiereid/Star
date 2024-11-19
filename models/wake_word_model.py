5064e906f3bdab6b0d771b20008c5b2515a4fe42
abbiereid <abbiereid222@gmail.com>
2024-11-19 15:59:28 +0000

    gesture recogniton working

--- models/wake_word_model.py
+++ models/wake_word_model.py
@@ -6,16 +6,30 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from camera import Camera
 from mediapipe.tasks import python
+import time
 from mediapipe.tasks.python import vision
-
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 def gesture_recognition(image):
-    return
-    
+    try:
+        with open('models\gesture_recognizer.task', 'rb') as file:
+            model = file_content = file.read()
+        base_options = python.BaseOptions(model_asset_buffer=model)
+        options = vision.GestureRecognizerOptions(base_options=base_options)
+        recognizer = vision.GestureRecognizer.create_from_options(options)
 
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        results = recognizer.recognize(mp_image)
+
+        if results.gestures:
+            return results.gestures[0][0]
+        else:
+            return "No gesture detected"
+    except Exception as e:
+        print(f"Error in gesture recognition: {e}")
+    
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     camera = Camera()
 
@@ -26,7 +40,7 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         image = cv2.flip(image, 1)
         image.flags.writeable = False
 
-        print(gesture_recognition(image))
+        print = gesture_recognition(image)
 
         try:
             results = hands.process(image)
@@ -36,8 +50,6 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        print(results)
 
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
