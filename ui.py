029d6a450c62cc7ec886fb46a01be68126952f60
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2024-11-25 17:11:11 +0000

    few changes for listening

--- ui.py
+++ ui.py
@@ -1,19 +1,20 @@
 import tkinter as tk
 from tkinter import PhotoImage
+import threading
 
 class UI:
     def __init__(self):
-        self.screen = tk.Tk(screenName="Star", baseName=None, className="UI")
+        self.screen = tk.Tk(screenName="Star", baseName=None, className="Star")
         self.screen.geometry("700x450")
 
-        self.logo = PhotoImage(file="assets/logo.png")
-        self.logo_label = tk.Label(self.screen, image=self.logo, bg='#c2d6d6')
-        self.logo_label.pack()
-
+        self.setLogo("assets/logo.png")
+        
         self.screen.config(bg='#c2d6d6')
-
 
     def run(self):
         self.screen.mainloop()
-
     
+    def setLogo(self, path):
+        self.logo = PhotoImage(file=path)
+        self.logo_label = tk.Label(self.screen, image=self.logo, bg='#c2d6d6')
+        self.logo_label.pack()
