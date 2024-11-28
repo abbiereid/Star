469601103c697d6dec1ae0338f7f31df09dc0ef3
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2024-11-28 00:00:49 +0000

    Utilised Pillow so I could access gif frame number, and dipslay each frame for the listenign gif

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import PhotoImage
+from PIL import Image, ImageTk
 import threading
 
 class UI:
@@ -14,10 +15,28 @@ class UI:
     def run(self):
         self.screen.mainloop()
     
-    def setLogo(self, path):
+    def setLogo(self, path): #This method is not really necessary anymore, may aswell simplifiy it.
         self.logo = PhotoImage(file=path)
         if hasattr(self, 'logo_label'):
             self.logo_label.config(image=self.logo)
         else:
             self.logo_label = tk.Label(self.screen, image=self.logo, bg='#c2d6d6')
             self.logo_label.pack()
+
+    def show_listening(self):
+        self.frames = []
+        self.gif = Image.open("assets/runningLine.gif")
+        
+        for frame in range(0, self.gif.n_frames):
+            self.gif.seek(frame)
+            self.frames.append(ImageTk.PhotoImage(self.gif.copy()))
+
+        self.listening_label = tk.Label(self.screen, bg='#c2d6d6')
+        self.listening_label.place(x=60, y=0)
+        self.update_listening_frames(0)
+
+    def update_listening_frames(self, i):
+        frame = self.frames[i]
+        i = (i + 1) % len(self.frames)
+        self.listening_label.config(image=frame)
+        self.screen.after(50, self.update_listening_frames, i)
