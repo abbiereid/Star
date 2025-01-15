eedbd4c68945af36d9f53b1c122a255e1b126ead
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2025-01-15 22:31:58 +0000

    listening animation done

--- ui.py
+++ ui.py
@@ -1,27 +1,35 @@
-import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import *
+from tkinter.ttk import *
 from PIL import Image, ImageTk
-import threading
 
 class UI:
     def __init__(self):
-        self.screen = tk.Tk(screenName="Star", baseName=None, className="Star")
-        self.screen.geometry("700x450")
+        self.screen = Tk()
+        self.screen.geometry("700x500")
+        self.canvas = Canvas(self.screen, width=700, height=500, bg='#c2d6d6')
+        self.logo = ImageTk.PhotoImage(Image.open("assets/logo.png"))
+        self.canvas.create_image(350, 225, image=self.logo)
+        self.canvas.pack()
 
-        self.setLogo("assets/logo.png")
-        
-        self.screen.config(bg='#c2d6d6')
+        self.lineX2 = 40
+        self.loadingLine = self.canvas.create_line(40, 450, self.lineX2, 450, fill="#6b9797", width=10) #X1 and X2 are the same so that it can't be seen
 
     def run(self):
         self.screen.mainloop()
-    
-    def setLogo(self, path): #This method is not really necessary anymore, may aswell simplifiy it.
-        self.logo = PhotoImage(file=path)
-        if hasattr(self, 'logo_label'):
-            self.logo_label.config(image=self.logo)
-        else:
-            self.logo_label = tk.Label(self.screen, image=self.logo, bg='#c2d6d6')
-            self.logo_label.pack()
 
     def show_listening(self):
-        pass
+        for i in range(60):
+            self.lineX2 += 10
+            self.move_line()
+        for i in range(60):
+            self.lineX2 -= 10
+            self.move_line()
+
+    def move_line(self):
+        self.canvas.coords(self.loadingLine, 40 , 450, self.lineX2, 450)
+        self.screen.update()
+        self.screen.after(10)
+
+ui = UI()
+ui.show_listening()
+ui.run()
