48b1c8c21e2cd11155414373f481afbf3c17e0ab
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2025-04-17 22:48:04 +0100

    assistant updates

--- models/assistant.py
+++ models/assistant.py
@@ -1,16 +1,25 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from ollama import chat
 
 class Assistant:
     def __init__(self):
-        self.path = "./llama-3.3-model"
-        self.tokenizer = AutoTokenizer.from_pretrained(self.path)
-        self.model = AutoModelForCausalLM.from_pretrained(self.path)
+        self.model = "llama3.2"
 
-    def receive_request(self, request):
-        pass
+    def recieveRequest(self, request):
+        self.processRequest(self.preprocessRequest(request))
 
-    def process_request(self, request):
-        pass
+    def preprocessRequest(self, request):
+        request= [
+            {"role": "system", "content": "You are a helpful assistant for non-verbal users, you will be given requests in ASL glosses, please translate them to English and respond in English."},
+            {"role": "user", "content": request}
+        ]
+        self.processRequest(request)
+    
 
-    def send_response(self, response):
-        pass
+    def processRequest(self, request):
+        try:
+            self.sendResponse(chat(self.model, request))
+        except Exception as e:
+            self.sendResponse(f"An error occurred: {e}")
+
+    def sendResponse(self, response):
+        print(response)
