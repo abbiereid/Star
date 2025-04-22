1f5beac2475fa76ac5008290a08b718bea285be3
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2025-04-22 15:52:31 +0100

    .

--- tests/models/test_wake_word_model.py
+++ tests/models/test_wake_word_model.py
@@ -5,39 +5,5 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from models.wake_word_model import WakeWordModel
 
-
 class TestWakeWordModel(unittest.TestCase):
-
-    def test_wakeWordModel_notifiesObservers_whenWakePhraseIsUsed(self):
-        # Arrange
-        model = WakeWordModel()
-        observer = MagicMock()
-        observer.subscribe(model)
-        # Act
-        model.notify(state=True)
-        # Assert
-        assert observer.notify.called()
-
-    def test_wakeWordModel_doesNotNotifyObservers_whenWakePhraseIsNotUsed():
-        # Arrange
-
-        # Act
-
-        # Assert
-        pass
-
-    def test_wakeWordModel_statesTrue_whenWakePhraseIsUsed():
-        # Arrange
-
-        # Act
-
-        # Assert
-        pass
-
-    def test_wakeWordModel_statesFalse_whenSleepPhraseIsUsed():
-        # Arrange
-
-        # Act
-
-        # Assert
-        pass
+    pass
