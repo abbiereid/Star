2e4dc9fc5604c780c6189f922f488e0a9eb9d07b
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2025-04-28 12:38:30 +0100

    metric

--- utils/weather.py
+++ utils/weather.py
@@ -15,7 +15,7 @@ class Weather:
             return "Ask Star to 'allow location' for weather details."
         
     async def get_weather(self, location):
-            async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+            async with python_weather.Client(unit=python_weather.METRIC) as client:
                 weather = await client.get(location.city)
                 return location.city + ", " + str(weather.temperature) + "°C , " + weather.description
 
